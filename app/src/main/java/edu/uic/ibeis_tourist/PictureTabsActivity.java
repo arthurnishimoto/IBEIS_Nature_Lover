2555ff9d67c0cb02a4bf37c4d5fd66c5783d1d90
Jed Siripun <jsiripun@gmail.com>
2015-04-23 22:21:13 -0500

    can open up tabs but they show up as full screen rather than within tab space

--- app/src/main/java/edu/uic/ibeis_tourist/PictureTabsActivity.java
+++ app/src/main/java/edu/uic/ibeis_tourist/PictureTabsActivity.java
@@ -1,12 +1,14 @@
 package edu.uic.ibeis_tourist;
 
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class PictureTabsActivity extends ActionBarActivity implements ActionBar.TabListener {
 
@@ -15,7 +17,7 @@ public class PictureTabsActivity extends ActionBarActivity implements ActionBar.
     // Declaring our tabs and the corresponding fragments.
     ActionBar.Tab picTab, mapTab;
     Fragment picFragTab = new MyPicturesFragment();
-    Fragment mapFragTab = new MyPicturesMapFragment();
+    Fragment mapFragTab = new edu.uic.ibeis_tourist.MyPicturesMapFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,24 +28,21 @@ public class PictureTabsActivity extends ActionBarActivity implements ActionBar.
         // Set the Action Bar to use tabs for navigation
         ActionBar ab = getSupportActionBar();
         ab.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-        picTab = ab.newTab();
-        //mapTab = ab.newTab();
-        picTab.setTabListener(new TabListener(picFragTab));
-        //mapTab.setTabListener(new TabListener(mapFragTab));
+        picTab = ab.newTab().setText("List View");
+        mapTab = ab.newTab().setText("Map View");
+        picTab.setTabListener(new edu.uic.ibeis_tourist.TabListener(picFragTab));
+        mapTab.setTabListener(new edu.uic.ibeis_tourist.TabListener(mapFragTab));
 
         // Add three tabs to the Action Bar for display
         ab.addTab(picTab);
-       // ab.addTab(mapTab);
-
-        ab.addTab(ab.newTab().setText("Map View").setTabListener(this));
+        ab.addTab(mapTab);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate menu from menu resource (res/menu/main)
-        getMenuInflater().inflate(R.menu.menu_settings, menu);
-
-        return super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.menu_my_pictures, menu);
+        return true;
     }
 
     // Implemented from ActionBar.TabListener
@@ -64,4 +63,17 @@ public class PictureTabsActivity extends ActionBarActivity implements ActionBar.
         // This is called when a previously selected tab is selected again.
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+
+            case R.id.action_settings:
+                Intent settingsView = new Intent(this, SettingsActivity.class);
+                startActivity(settingsView);
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
